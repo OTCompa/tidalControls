10e9c45dca4171aa966b0ba8eaf76e25024b15c8
OTCompa <41562938+OTCompa@users.noreply.github.com>
2025-06-23 22:47:48 -0500

    update authors

--- index.tsx
+++ index.tsx
@@ -1,19 +1,7 @@
 /*
- * Vencord, a modification for Discord's desktop app
- * Copyright (c) 2022 Vendicated and contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 import { Settings } from "@api/Settings";
@@ -32,13 +20,13 @@ function toggleHoverControls(value: boolean) {
 export default definePlugin({
   name: "TidalControls",
   description: "Adds a Tidal player above the account panel",
-  authors: [Devs.Ven, Devs.afn, Devs.KraXen72, Devs.Av32000, Devs.nin0dev],
+  authors: [Devs.Ven, Devs.afn, Devs.KraXen72, Devs.Av32000, Devs.nin0dev, { name: "Frey", id: 123124171386388480n }],
   options: {
     hoverControls: {
       description: "Show controls on hover",
       type: OptionType.BOOLEAN,
       default: false,
-      onChange: (v) => toggleHoverControls(v),
+      onChange: v => toggleHoverControls(v),
     },
     useSpotifyUris: {
       type: OptionType.BOOLEAN,
